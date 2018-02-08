3103f7b5d6b240e568df7004883dad6cf97fb103
Nguyen Thanh Duc <duc.nguyen@2359media.com>
2018-02-08 17:11:36 +0700

    initial commit

---
+++ ThreeRingControl/ThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControl.h
+//  ThreeRingControl
+//
+//  Created by Nguyen Thanh Duc on 8/2/18.
+//  Copyright © 2018 Nguyen Thanh Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControl.
+FOUNDATION_EXPORT double ThreeRingControlVersionNumber;
+
+//! Project version string for ThreeRingControl.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControl/PublicHeader.h>
+
+
